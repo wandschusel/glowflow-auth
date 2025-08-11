8cd8ddef4be972efe9d417c513bc365b5343f097
wandschusel <90412748+wandschusel@users.noreply.github.com>
2025-08-10 19:55:13 -0700

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+import type { NextConfig } from "next";
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: { serverActions: { allowedOrigins: ["*"] } }
+};
+export default nextConfig;
